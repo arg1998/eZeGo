28becbc5c1062df2ba2459ac8e4b5b98089a3592
ARGOSTA <arg7971@gmail.com>
2024-10-18 23:21:43 -0500

    implemented a basic runtime assertion mechanism

---
+++ src/core/assertion/assertions.hpp
@@ -0,0 +1,37 @@
+#include "definitions.hpp"
+
+
+#if defined(EZ_COMPILER_CLANG)
+    #define ezDebugBreak() __builtin_trap() 
+#elif defined(EZ_COMPILER_MSVC)
+    #define ezDebugBreak() __debugbreak()
+#endif
+
+void report_assertion_failure(const char *expression, const char* message, const char *file, s32 line);
+
+
+#if (EZ_CONFIG_ASSERTION_ENABLED == true)
+
+    #define EZ_ASSERT(expression)                                                           \
+        {                                                                                   \
+            if (expression) {                                                               \
+            } else {                                                                        \
+                report_assertion_failure(#expression, "", __FILENAME__, __LINE__);          \
+                ezDebugBreak();                                                             \
+            }                                                                               \
+        }
+
+    #define EZ_ASSERT_MSG(expression, message)                                              \
+        {                                                                                   \
+            if(expression){                                                                 \
+            } else {                                                                        \
+                report_assertion_failure(#expression, message, __FILENAME__, __LINE__);     \
+                ezDebugBreak();                                                             \
+            }                                                                               \
+        }
+
+
+#else
+    #define EZ_ASSERT(expression)                   // nothing
+    #define EZ_ASSERT_MSG(expression, message)      // nothing
+#endif
--- src/core/logger/logger.cpp
+++ src/core/logger/logger.cpp
@@ -1,4 +1,5 @@
 #include "logger.hpp"
+#include "core/assertion/assertions.hpp"
 
 #include <iostream>
 #include <cstdarg>
@@ -31,5 +32,14 @@ void logOutout(LogLevel log_level, const std::string message, const char *_file,
     #else
         std::cout << "[" << level_strings[log_level] << "]: " << messageBuffer << " <" << _file << ":L" << _line << ">" << std::endl;
     #endif
+}
+
+void report_assertion_failure(const char *expression, const char* message, const char *file, s32 line){
+    if(message[0] == '\0'){
+        logOutout(EZ_LOG_LEVEL_FATAL, "ASSERTION FAILURE  --> Expression: %s --> File:", file, line, expression);
+    } else {
+        logOutout(EZ_LOG_LEVEL_FATAL, "ASSERTION FAILURE  -->  Expression: %s  -->  Message: %s  -->  File:", file, line, expression, message);
+    }
+    
 }
 
